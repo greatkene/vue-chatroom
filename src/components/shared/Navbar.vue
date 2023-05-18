3b776342de4351d3fe3ccc52fd18be68522e59bb
greatkene <ihevuemeg@gmail.com>
2023-05-18 01:21:44 +0100

    feat: navbar setup

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,5 +1,34 @@
 <template>
-   <nav>
-       <RouterLink to="/">Home</RouterLink>
-   </nav>
+    <nav>
+        <div class="container">
+            <h1>Chatroom</h1>
+            <div class="menu">
+                <RouterLink to="/" class="is-active">Home</RouterLink>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/">Home</RouterLink>
+            </div>
+            <button :class="['hamburger', { 'is-active': isNavbarActive }]" @click="toggleNavbar">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+    </nav>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isNavbarActive: false
+        };
+    },
+    methods: {
+        toggleNavbar() {
+            this.isNavbarActive = !this.isNavbarActive;
+        }
+    }
+};
+
+</script>
