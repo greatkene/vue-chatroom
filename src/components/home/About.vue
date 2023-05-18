c71234faf3be66b4478eef15fd7aa80840a32a6a
greatkene <ihevuemeg@gmail.com>
2023-05-18 17:38:31 +0100

    feat: about section

---
+++ src/components/home/About.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="section about" id="about" aria-label="about">
+        <div class="container">
+
+            <figure class="about-banner">
+                <img src="../../assets/images/about-sec.svg" loading="lazy" alt="about banner">
+            </figure>
+
+            <div class="about-content">
+                <p class="section-subtitle">Our Acquaintance</p>
+                <h2 class="section-title">Chat Moderation</h2>
+                <p class="section-text">
+                    Immerse yourself in a dynamic community of like-minded individuals, where lively discussions take place
+                    around the clock. Explore an array of chat rooms tailored to various topics, from hobbies and
+                    entertainment to technology and beyond.
+                </p>
+
+                <ul class="about-list">
+                    <li class="about-item">
+                        <div class="item-icon">
+                            <ion-icon name="folder" aria-hidden="true"></ion-icon>
+                        </div>
+
+                        <div>
+                            <h3 class="item-title">Stay Connected In Real-Time</h3>
+
+                            <p class="item-text">
+                                Lorem ipsum dolor sit amet, consec tetur adipiscing elit Lorem ipsum dolor sit amet, consec
+                                tetur
+                                adipiscing elit.
+                            </p>
+                        </div>
+                    </li>
+
+                    <li class="about-item">
+                        <div class="item-icon">
+                            <ion-icon name="pie-chart" aria-hidden="true"></ion-icon>
+                        </div>
+
+                        <div>
+                            <h3 class="h3 item-title">Easy to Navigate</h3>
+
+                            <p class="item-text">
+                                Lorem ipsum dolor sit amet, consec tetur adipiscing elit Lorem ipsum dolor sit amet, consec
+                                tetur
+                                adipiscing elit.
+                            </p>
+                        </div>
+
+                    </li>
+
+                </ul>
+
+                <a href="#" class="btn btn-secondary">Discover More</a>
+
+            </div>
+
+        </div>
+    </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,17 @@
   <main>
     <Hero />
     <Features />
+    <About />
   </main>
 </template>
 
 <script>
+import About from '../components/home/About.vue';
 import Features from '../components/home/Features.vue';
 import Hero from '../components/home/Hero.vue';
 
 export default {
-  components: { Hero, Features },
+  components: { Hero, Features, About },
   name: 'Home'
 }
 </script>
