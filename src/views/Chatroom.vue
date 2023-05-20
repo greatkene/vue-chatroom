748c0b0ecb99ae0e40793ad04e248a158f76b77a
greatkene <ihevuemeg@gmail.com>
2023-05-20 11:13:04 +0100

    feat: redirect user to homepage onLogout

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -13,8 +13,22 @@ import ChatHeader from '../components/chatroom/ChatHeader.vue';
 import ChatBody from '../components/chatroom/ChatBody.vue'
 import ChatFooter from '../components/chatroom/ChatFooter.vue'
 
+import getUser from '../composables/getUser';
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
+
 export default {
     components: { ChatHeader, ChatBody, ChatFooter },
-    name: 'Chatroom'
+    name: 'Chatroom',
+    setup() {
+        const { user } = getUser()
+        const router = useRouter()
+
+        watch(user, () => {
+            if (!user.value) {
+                router.push('/')
+            }
+        })
+    }
 }
 </script>
