a6b75fe5c6bbbe8d8c959400b7b7fbfae45f3fd6
greatkene <ihevuemeg@gmail.com>
2023-05-21 16:34:52 +0100

    feat: added automatic scroll to chat  body

--- src/components/chatroom/ChatBody.vue
+++ src/components/chatroom/ChatBody.vue
@@ -2,8 +2,8 @@
     <div>
         <div v-if="error">{{ error }}</div>
 
-        <div v-if="documents" class="chat-container">
-            <div v-for="doc in documents" :key="doc.id" class="chat-body">
+        <div class="chat-container" ref="container">
+            <div class="chat-body" v-for="doc in documents" :key="doc.id" ref="messages">
                 <span class="created-at">{{ formatDate(doc.createdAt) }}</span>
                 <span class="chat-name">{{ doc.name }}</span>
                 <p class="chat-message">
@@ -17,6 +17,7 @@
 <script>
 import getCollection from '../../composables/getCollection';
 import moment from 'moment';
+import { ref, watch, nextTick } from 'vue';
 
 export default {
     setup() {
@@ -26,7 +27,27 @@ export default {
             return moment(date.toDate()).fromNow();
         };
 
-        return { error, documents, formatDate };
+        const container = ref(null);
+        const messages = ref([]);
+
+        watch(
+            documents,
+            () => {
+                nextTick(() => {
+                    scrollToBottom();
+                });
+            },
+            { immediate: true }
+        );
+
+        const scrollToBottom = () => {
+            if (container.value && messages.value.length > 0) {
+                const lastMessage = messages.value[messages.value.length - 1];
+                lastMessage.scrollIntoView({ behavior: 'smooth', block: 'end' });
+            }
+        };
+
+        return { error, documents, formatDate, container, messages };
     },
 };
 </script>
